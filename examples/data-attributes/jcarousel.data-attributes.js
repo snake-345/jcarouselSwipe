aa59cfefa9279ad2e4142005e1f1afd4b2a4b942
Evgeniy Pelmenev <g.snake345@gmail.com>
2015-12-09 11:10:33 +0200

    add data attributes examples

--- examples/data-attributes/jcarousel.data-attributes.js
+++ examples/data-attributes/jcarousel.data-attributes.js
@@ -5,6 +5,11 @@
             el.jcarousel(el.data());
         });
 
+        $('[data-jcarousel-swipe]').each(function() {
+            var el = $(this);
+            el.jcarouselSwipe();
+        });
+
         $('[data-jcarousel-control]').each(function() {
             var el = $(this);
             el.jcarouselControl(el.data());
