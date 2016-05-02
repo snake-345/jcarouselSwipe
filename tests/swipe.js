107916d5aa88c2a5025957788d01845dea2a6f78
Evgeniy Pelmenev <g.snake345@gmail.com>
2016-05-02 13:54:13 +0200

    write simple test

--- tests/swipe.js
+++ tests/swipe.js
@@ -1,6 +1,11 @@
 Feature('Check swipes');
 
-Scenario('Check work next buttons', (I) => {
-	I.amOnPage('/examples/basic/');
+Scenario('Check work swipes', (I) => {
+	I.amOnPage('/tests/pages/default/');
 	I.swipe('.jcarousel', -100, 0);
+	I.waitToHide('.slide-1', 2);
+	I.seeElement('.slide-2');
+	I.swipe('.jcarousel', -100, 0);
+	I.waitToHide('.slide-2', 2);
+	I.seeElement('.slide-3');
 });
