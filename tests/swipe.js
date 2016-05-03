66e2ac56c2deb30eee79c72a08d21472735a3ed0
Evgeniy Pelmenev <g.snake345@gmail.com>
2016-05-03 22:30:43 +0200

    add some tests

--- tests/swipe.js
+++ tests/swipe.js
@@ -8,4 +8,16 @@ Scenario('Check work swipes', (I) => {
 	I.swipe('.jcarousel', -100, 0);
 	I.waitToHide('.slide-2', 2);
 	I.seeElement('.slide-3');
+	I.swipe('.jcarousel', 100, 0);
+	I.waitToHide('.slide-3', 2);
+	I.seeElement('.slide-2');
+	I.swipe('.jcarousel', 100, 0);
+	I.waitToHide('.slide-2', 2);
+	I.seeElement('.slide-1');
+	I.swipe('.jcarousel', -1000, 0);
+	I.waitToHide('.slide-2', 2);
+	I.seeElement('.slide-3');
+	I.swipe('.jcarousel', -1000, 0);
+	I.waitToHide('.slide-4', 2);
+	I.seeElement('.slide-5');
 });
